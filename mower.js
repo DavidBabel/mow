022f2c62e87916a60093984c1353c3fafbe8c4a1
David B <babel.david@gmail.com>
2018-06-29 14:44:14 +0200

    first commit

---
+++ mower.js
@@ -0,0 +1,4 @@
+
+// write your mower program here
+
+
---
+++ test/mower.test.js
@@ -0,0 +1,8 @@
+
+// assert.deepEqual
+
+describe('MowItNow', () => {
+  it('should initialise and return the mower position', () => {
+
+  });
+});
