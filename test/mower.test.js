6343ec80e4d9cb8fce6fd57c1052979047b55002
David Babel <DavidBabel@users.noreply.github.com>
2019-02-20 18:00:35 +0100

    Update mower.test.js

--- test/mower.test.js
+++ test/mower.test.js
@@ -2,16 +2,45 @@
 // assert.deepEqual
 
 describe('MowItNow', () => {
+
   it('should initialise and return the mower position', () => {
 
   });
+  
+//  it('should move the mower north', () => {
+//    // MM
+//    
+//  });
+  
+//  it('should rotate the mower right', () => {
+//    // RRR
+//
+//  });
+  
+//  it('should rotate the mower left', () => {
+//    // LLL
+//    
+//  });
+  
+//  it('should be able to move the mower in every directions', () => {
+//    // MRMRMRMR
+//    
+//  });
+  
+//  it('should keep the mower in the allowed coordinate area', () => {
+//    // LMMLMMM
+//    
+//  });
+
+//  it('yeah, everything seems to work as expected', () => {
+//    // LLMMMRRRRRMMRMMMRMLM
+//    
+//  });
+  
 });
 
 
 
-// MM
-// RRR
-// RRRMRRM
-// LMMM
-// MRMRMLMRMMM
-// LLMRMMRMMMRMLM
+
+
+
